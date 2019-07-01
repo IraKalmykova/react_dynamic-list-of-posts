4d44b1f8ef689ac093b01530f077f97cad81db1c
Ira <kalmykova_ira@ukr.net>
2019-07-01 15:53:38 +0300

    Updates

---
+++ precache-manifest.1dbaa7e7af6df4146450484113782787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acc45a6b6ce82974d619c1c33ea165b",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "36222ff0ad993bc00703",
+    "url": "/react_dynamic-list-of-posts/static/css/main.7e356545.chunk.css"
+  },
+  {
+    "revision": "33ed0e6b37df14901f68",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a13ca59e.chunk.js"
+  },
+  {
+    "revision": "36222ff0ad993bc00703",
+    "url": "/react_dynamic-list-of-posts/static/js/main.d4bad56e.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
